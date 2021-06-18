1585fe1aac2d023fc0f37258cc2d7a086093694d
Jesse Stanger <jesse.stanger@gmail.com>
2021-06-18 21:09:38 +1000

    connect to phizzi chat

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,43 @@
-console.log('Hello world')
+import { config as DotEnvConfig } from 'dotenv'
+import { ChatUserstate, Client, Options } from 'tmi.js'
+
+DotEnvConfig()
+
+const options: Options = {
+  identity: {
+    username: process.env.AUTH_USERNAME,
+    password: `oauth:${process.env.OAUTH_TOKEN}`,
+  },
+  channels: ['phizzi'],
+  options: {
+    debug: true,
+  },
+  connection: {
+    reconnect: true,
+    secure: true,
+  },
+}
+
+const client = new Client(options)
+
+/** Actions to perform when a new message is received */
+const onMessageHandler = (
+  channel: string,
+  userState: ChatUserstate,
+  msg: string,
+  isSelf: boolean,
+) => {
+  if (isSelf) {
+    return
+  }
+  console.log(userState)
+}
+
+function onConnectedHandler(addr: string, port: number) {
+  console.log(`Connected to ${addr}:${port}`)
+}
+
+client.on('message', onMessageHandler)
+client.on('connected', onConnectedHandler)
+
+client.connect().catch((e) => console.error('\nServer crashed with:', e))
