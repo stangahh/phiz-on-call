1ddd56dd5261ef4372842a15a3cbb460b7fcbc15
Jesse Stanger <jesse.stanger@gmail.com>
2021-06-19 15:37:38 +1000

    add express server for web-app, add pug

--- projects/web-app/src/index.ts
+++ projects/web-app/src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello world')
+import compression from 'compression'
+import express, { Request, Response } from 'express'
+import path from 'path'
+
+const app = express()
+app.set('port', process.env.PORT || 3000)
+app.set('views', path.join(__dirname, './views'))
+app.set('view engine', 'pug')
+app.use(compression())
+
+app.get('/', (req: Request, res: Response) => {
+  res.render('home.pug', {
+    title: 'Home',
+  })
+})
+
+app.listen(app.get('port'), () => {
+  console.log(`App is running at http://localhost:${app.get('port')}`)
+})
