1bbe7c5f19643c8f85301e1f9918f2804d62037a
Jesse Stanger <jesse.stanger@gmail.com>
2021-07-10 21:10:24 +1000

    rm debug commands, trigger on phizCall

--- projects/chat-poller/src/helpers/callAction.ts
+++ projects/chat-poller/src/helpers/callAction.ts
@@ -5,25 +5,27 @@ export const callAction = async (
   target: string,
   tts: string,
 ) => {
+  // TODO: Add check for subscriber is T3
+
   // ACTUAL
-  if (emote.match(/^START$/)) {
+  if (emote.match(/^phizCall$/)) {
     await sendAction('ringing', target, tts)
   }
 
   // MOCK
-  if (emote.match(/^ACCEPT$/)) {
-    await sendAction('answered', target, tts)
-  }
+  // if (emote.match(/^ACCEPT$/)) {
+  //   await sendAction('answered', target, tts)
+  // }
 
-  if (emote.match(/^DENY$/)) {
-    await sendAction('message-bank', target, tts)
-  }
+  // if (emote.match(/^DENY$/)) {
+  //   await sendAction('message-bank', target, tts)
+  // }
 
-  if (emote.match(/^HANG$/)) {
-    await sendAction('hang', target, tts)
-  }
+  // if (emote.match(/^HANG$/)) {
+  //   await sendAction('hang', target, tts)
+  // }
 
-  if (emote.match(/^RESET$/)) {
-    await sendAction('reset', target, tts)
-  }
+  // if (emote.match(/^RESET$/)) {
+  //   await sendAction('reset', target, tts)
+  // }
 }
