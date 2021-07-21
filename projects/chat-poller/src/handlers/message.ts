fd4da1c7afa4bcddd6c99c61a291e08ec4229183
Jesse Stanger <jesse.stanger@gmail.com>
2021-07-21 22:12:50 +1000

    i am god (free call ins EZ)

--- projects/chat-poller/src/handlers/message.ts
+++ projects/chat-poller/src/handlers/message.ts
@@ -3,6 +3,9 @@ import { callAction } from '../helpers/callAction.js'
 import { parseMessage } from '../helpers/parseMessage.js'
 import { getRandomNumber } from '../helpers/random.js'
 
+/** Users who can skip the t3 check */
+const GODS = ['stangahh', 'phizzi']
+
 /** Actions to perform when a new message is received */
 export const onMessageHandler = async (
   channel: string,
@@ -15,8 +18,9 @@ export const onMessageHandler = async (
   }
 
   const isTier3 = userState.badges?.subscriber?.startsWith('30')
+  const isGod = GODS.includes(userState.username || '')
 
-  if (!isTier3) {
+  if (!isTier3 && !isGod) {
     return
   }
 
