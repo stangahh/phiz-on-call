622ef9153f36c950013895c58afec4a813e460db
Phizzi <phiziam@gmail.com>
2022-09-13 20:09:28 +1000

    update override users

--- projects/chat-poller/src/handlers/message.ts
+++ projects/chat-poller/src/handlers/message.ts
@@ -4,7 +4,7 @@ import { parseMessage } from '../helpers/parseMessage.js'
 import { getRandomNumber } from '../helpers/random.js'
 
 /** Users who can skip the t3 check */
-const GODS = ['stangahh', 'phizzi']
+const GODS = ['stangahh', 'phizzi', 'thegestoord', 'mayoketchup', 'toshichatzalot']
 
 /** Actions to perform when a new message is received */
 export const onMessageHandler = async (
